268a6e7928976a817c824553c21decafd8b73050
BenjaminSymons <mail@bensymons.com>
2021-07-22 11:24:52 +0800

    Adding CORS to the API

--- course.js
+++ course.js
@@ -6,6 +6,14 @@ export const create = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body)
 
+  const user = event.requestContext?.identity.cognitoIdentityId
+
+  if (user) {
+    console.log(user)
+  } else {
+    console.log('No user in use')
+  }
+
   const courseID = 'COURSE#' + uuid.v4()
 
   const params = {
